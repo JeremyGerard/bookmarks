8531dd9ecd670b8b546c20d29e050bb7b349cbb6
JeremyGerard <jerem.gerard@gmail.com>
2016-09-15 09:13:01 +0000

    Gestion utilisateur

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,14 @@
 class Bookmark < ActiveRecord::Base
+	belongs_to :user
+
+	#vérifier d'un utilisateur est connecté
+	validates :user_id, presence: true
+
+	#Vérifier que le titre est présent et qu'il fait au moins 5 caractères
+	validates :title, length: {minimum: 5}, presence: true
+
+	
+
+	
+
 end
